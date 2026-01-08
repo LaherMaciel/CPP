5cf20ed2bbe61de12b1f7780d60af826311b5850
Laher Maciel <Laherwpayotmaciel@gmail.com>
2026-01-08 14:43:52 +0000

    send

--- CPP_Module_00/ex01/src/MyAwesomePhoneBook.cpp
+++ CPP_Module_00/ex01/src/MyAwesomePhoneBook.cpp
@@ -1,12 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   MyAwesomePhoneBook.cpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lahermaciel <lahermaciel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 15:03:17 by lahermaciel       #+#    #+#             */
-/*   Updated: 2025/09/15 15:03:18 by lahermaciel      ###   ########.fr       */
+/*   Updated: 2025/12/02 15:33:01 by lahermaciel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Welcome to My Awesome Phone Book!" << std::endl;
+	return 0;
+}
