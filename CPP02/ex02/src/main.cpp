35d8e9665feb78af2a19b09aa3ef80c57f959095
LaherMaciel <Laherwpayotmaciel@gmail.com>
2026-04-06 13:50:02 +0100

    upgrade in the CPP02/ex02 main.

--- CPP02/ex02/src/main.cpp
+++ CPP02/ex02/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lahermaciel <lahermaciel@student.42.fr>    +#+  +:+       +#+        */
+/*   By: lawences <lawences@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 09:14:05 by lahermaciel       #+#    #+#             */
-/*   Updated: 2026/03/09 20:33:44 by lahermaciel      ###   ########.fr       */
+/*   Updated: 2026/04/06 13:41:12 by lawences         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ int main( void )
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	Fixed c(5);
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	/* std::cout << c << std::endl;
-	std::cout << c + a << std::endl;
-	std::cout << c - a << std::endl;
-	std::cout << c * a << std::endl;
-	std::cout << c / a << std::endl; */
-	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "	a = " << a << std::endl;
+	std::cout << "	++a = " << ++a << std::endl;
+	std::cout << "	a = "  << a << std::endl;
+	std::cout << "	a++ = " << a++ << std::endl;
+	std::cout << "	a = " << a << std::endl;
+	std::cout << "	b = " << b << std::endl;
+	std::cout << "	c = " << c << std::endl;
+	std::cout << "	c + a = " << c << " + " << a << " = "<< c + a << std::endl;
+	std::cout << "	c - a = " << c << " - " << a << " = " << c - a << std::endl;
+	std::cout << "	c * a = " << c << " * " << a << " = " << c * a << std::endl;
+	std::cout << "	c / a = " << c << " / " << a << " = " << c / a << std::endl;
+	std::cout << "	" << Fixed::max( a, b ) << std::endl;
+	std::cout << "	" << Fixed::min( a, b ) << std::endl;
 
 	return (0);
 }
